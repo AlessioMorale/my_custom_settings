5fc33f619540485ab0344a9dbbb7bba456957274
Alessio Morale <alessiomorale@gmail.com>
2023-01-28 12:12:32 +0000

    fix: add exa, fix apt install, fix powerline, cleanup git aliases

--- gitconfig_alias.sh
+++ gitconfig_alias.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e
 
-git config --global alias.s "status"
-git config --global alias.co "checkout"
 git config --global alias.b "checkout -b"
-git config --global alias.del "branch -D"
 git config --global alias.br 'branch --format="%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]" --sort=-committerdate'
-git config --global alias.save "!git add -A && git commit -m 'chore: savepoint'"
+git config --global alias.cm "commit -m"
+git config --global alias.co "checkout"
+git config --global alias.d "diff"
+git config --global alias.del "branch -D"
+git config --global alias.done "push origin HEAD"
+git config --global alias.l 'log --pretty=format:"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]" --abbrev-commit -30'
+git config --global alias.last "log -1 HEAD --stat"
+git config --global alias.ll "log --oneline"
+git config --global alias.p "push"
 git config --global alias.pushf "push --force-with-lease"
+git config --global alias.res "reset --hard"
+git config --global alias.rv "remote -v"
+git config --global alias.s "status"
+git config --global alias.save "!git add -A && git commit -m 'chore: savepoint'"
+git config --global alias.st "status -sb"
 git config --global alias.undo "reset HEAD~1 --mixed"
-git config --global alias.res "!git reset --hard"
-git config --global alias.done "!git push origin HEAD"
-git config --global alias.l '!git log --pretty=format:"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]" --abbrev-commit -30'
--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-sudo apt update || true
-sudo apt install curl
+sudo apt update -q || true
+sudo apt install curl direnv exa -y --no-install-recommends
 
 echo Installing sdkman
 curl -s "https://get.sdkman.io" | bash
+# shellcheck disable=SC1091
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
 
@@ -15,10 +18,12 @@ mkdir -p ~/.local/share/blesh
 cp -Rf ble-nightly/* ~/.local/share/blesh/
 rm -rf ble-nightly
 
-
 echo installing powerline
 pip3 install powerline-shell
 
-grep -qxF 'source "$HOME/my_custom_settings/bashrc_addons"' "$HOME/.bashrc" || echo 'source "$HOME/my_custom_settings/bashrc_addons"' >> "$HOME/.bashrc"
+SOURCE_LINE="source \"$SCRIPTPATH/bashrc_addons\""
+echo adding custom config to .bashrc: "$SOURCE_LINE"
+grep -qxF "$SOURCE_LINE" "$HOME/.bashrc"|| echo "$SOURCE_LINE" >> "$HOME/.bashrc"
 
+echo setting up git aliases
 ./gitconfig_alias.sh
